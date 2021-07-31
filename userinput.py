e50ba5b48363a1ccac77a47f2ff8a8eedc60cbdf
Aadesh Dulal <offaadeshdulal@gmail.com>
2021-07-31 20:31:17 +0545

    Added more learning files

--- userinput.py
+++ userinput.py
@@ -1,4 +1,3 @@
     # Store input numbers:  
-    num1 = input('Enter first number: ')  
-    num2 = input('Enter second number: ')  
-      
+num1 = input('Enter first number: ')  
+num2 = input('Enter second number: ')
